cc8a7ae370a72951fbe2b532371d27adc4b55db2
Billy <billy.xu99@gmail.com>
2023-11-06 21:00:49 +1000

    Added id to clear and equals. Added function to Javascript for calculations

---
+++ main.js
@@ -0,0 +1,24 @@
+const buttons = document.querySelectorAll('button')
+const screenDisplay = document.querySelector('.screen')
+
+let calculation = []
+
+function calculate(button) {
+    const value = button.textContent
+    if (value == "clear") {
+        calculation = []
+        screenDisplay.textContent = '.'
+    } else if (value === "=") {
+        screenDisplay.textContent = eval(accumulativeCalculation)
+    } else {
+        calculation.push(value);
+        accumulativeCalculation = calculation.join('')
+        screenDisplay.textContent = accumulativeCalculation
+    }
+
+}
+
+
+
+
+buttons.forEach(button => button.addEventListener('click', () => calculate(button)))
